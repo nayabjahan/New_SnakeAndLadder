b033749c7a6767e774894d24da22dee8d8069e74
nayab jahan <nayabzafir@gmail.com>
2022-08-05 20:54:20 +0530

    Added:SnakeAndLadderGame

---
+++ src/main/java/com/blz/SnakeAndLadder/SnakeAndLadder.java
@@ -0,0 +1,4 @@
+package com.blz.SnakeAndLadder;
+
+public class SnakeAndLadder {
+}
---
+++ src/main/java/com/blz/SnakeAndLadder/SnakeAndLadderMain.java
@@ -0,0 +1,8 @@
+package com.blz.SnakeAndLadder;
+
+public class SnakeAndLadderMain {
+    public static void main(String[] args) {
+        System.out.println("welcome to snake and ladder game");
+
+    }
+}
