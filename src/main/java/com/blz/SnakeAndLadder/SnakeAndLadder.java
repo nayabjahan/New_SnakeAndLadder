2ec98b66cb9e258683340a84e218976fd9942182
nayab jahan <nayabzafir@gmail.com>
2022-08-06 19:54:01 +0530

    Added:player1 position is 0

--- src/main/java/com/blz/SnakeAndLadder/SnakeAndLadder.java
+++
@@ -1,4 +0,0 @@
-package com.blz.SnakeAndLadder;
-
-public class SnakeAndLadder {
-}
--- src/main/java/com/blz/SnakeAndLadder/SnakeAndLadderMain.java
+++ src/main/java/com/blz/SnakeAndLadder/SnakeAndLadderMain.java
@@ -3,6 +3,10 @@ package com.blz.SnakeAndLadder;
 public class SnakeAndLadderMain {
     public static void main(String[] args) {
         System.out.println("welcome to snake and ladder game");
+        int position = 0;
+        System.out.println("player1 position is " + position);
+        int player1 = (int)((Math.random()*10)%1);
+        System.out.println(player1);
 
     }
 }
