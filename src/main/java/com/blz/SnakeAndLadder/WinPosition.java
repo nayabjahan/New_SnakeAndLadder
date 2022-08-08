0422eb08bd9ed295b740415fe8d52d116ca7792c
nayab jahan <nayabzafir@gmail.com>
2022-08-08 21:48:13 +0530

    modified:Win position and Current position of player

--- src/main/java/com/blz/SnakeAndLadder/WinPosition.java
+++ src/main/java/com/blz/SnakeAndLadder/WinPosition.java
@@ -1,44 +1,40 @@
 package com.blz.SnakeAndLadder;
 
 public class WinPosition {
-    public static final int noplay = 0;
-    public static final int ladder = 1;
-    public static final int snake = 2;
+    final int ladder = 2;
+    final int snake = 1;
 
     public void winPosition() {
-        int position = 0, final_position = 100, count = 0;
-        while (position < final_position) {
-            count++;
-            System.out.println("player position is :" + position);
-
-//generating random number from 1 to 6
+        int position = 0;
+        while (position <= 100) {
             int roll_dice = (int) (Math.floor(Math.random() * 10) % 6) + 1;
             int option = (int) (Math.floor(Math.random() * 10) % 3);
-            //check for options like noplay,ladder,snake
+            //check for options like ladder,snake
             switch (option) {
-                case noplay:
-                    //position = 0;
-                    System.out.println("its a noplay! stays here in the same place");
-                    break;
                 case ladder:
-                    position += roll_dice;
-                    System.out.println("its a ladder! player moves ahead by" + roll_dice);
+                    System.out.println("rolling dice ...... " + roll_dice);
+                    System.out.println("Option is ...... " + option);
+                    position = position + roll_dice;
+                    if (position > 100) {
+                        position = 100;
+                    }
+                    System.out.println("Its a Ladder & Your Position Goes to " + position);
                     break;
                 case snake:
-                    if ((position != 0) && (position > roll_dice)) {
-                        position -= roll_dice;
-                        System.out.println("ita a snake! player moves behind by" + roll_dice);
-                    } else {
-                        position = 0;
-                        System.out.println("its snake ! player moves behind by" + roll_dice);
+                    System.out.println("rolling dice ...... " + roll_dice);
+                    System.out.println("Option is ...... " + option);
+                    position = position - roll_dice;
+                    System.out.println("Its a Snake & Your Position Goes to " + position);
+                    if (position == 0) {
+                        System.out.println("Player position is Same " + position);
                     }
                     break;
                 default:
-                    System.out.println("Error! please check");
+                    System.out.println("Error! try again");
                     break;
             }
-        }
-        System.out.println("current position of the player in the game is :" + position);
+            position++;
 
+        }
     }
 }
